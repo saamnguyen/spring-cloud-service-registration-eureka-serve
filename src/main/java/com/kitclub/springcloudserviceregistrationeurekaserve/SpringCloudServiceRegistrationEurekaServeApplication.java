59e5a64b2e21eb7326f3a0e10223906c299fa82b
samukma <nguyensam576@gmail.com>
2021-11-09 16:08:56 +0700

    create proj

---
+++ src/main/java/com/kitclub/springcloudserviceregistrationeurekaserve/SpringCloudServiceRegistrationEurekaServeApplication.java
@@ -0,0 +1,13 @@
+package com.kitclub.springcloudserviceregistrationeurekaserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServiceRegistrationEurekaServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServiceRegistrationEurekaServeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kitclub/springcloudserviceregistrationeurekaserve/SpringCloudServiceRegistrationEurekaServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitclub.springcloudserviceregistrationeurekaserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServiceRegistrationEurekaServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
